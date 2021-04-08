52d5413ecd6cdb93d09c64efc49affe420ac156a
Amin Adibi <adibi@alumni.ubc.ca>
2021-04-07 21:56:05 -0700

    model added to paper

---
+++ analysis/main_figures.R
@@ -0,0 +1,232 @@
+library(Cairo)
+source('./analysis/setup.R')
+
+# parameter space
+pars <- crossing(R=c(1.15, 1.3, 1.4, 1.5), ve = c(0.6,0.75,0.9), vp = 0.8, scen=c(1,2))
+
+# RUN (according to piecewise scenario)
+res <- pars %>%  future_pmap_dfr(run_over_scen_2, .progress=TRUE)
+
+############################
+# FIGURE 1 (trajectories)
+#############################
+# Look at trajectories
+trajA <- compare_sims(sim1 = filter(res, R==1.3 & scen==1 & ve==0.75), 
+                             sim2=filter(res, R==1.3 & scen ==2 & ve==0.75),
+                             name1=labels[1], name2=labels[2], startDate=startDate, 
+                             textsize = 16)
+
+# trajB <- compare_sims(sim1 = filter(res, R==1.3 & scen==3 & ve==0.75), 
+#                              sim2=filter(res, R==1.3 & scen==4 & ve==0.75),
+#                              name1=labels[3], name2=labels[4], startDate=startDate, 
+#                              textsize = 16)
+
+# Look at number vaccinated
+gg_vax <- res %>% 
+    filter(R==1.15) %>% # note: all R values will give approx. the same plot
+    group_by(type)%>% 
+    nest()%>%
+    summarize(plot=map(data, display_prop_vax, startDate, type, textsize=16))
+
+
+fig1a = ggarrange(gg_vax$plot[[1]]+
+                    labs(subtitle = 'A: 80+, 70-79, EW, 60-69, ...') +
+                    scale_color_viridis(discrete = T)+
+                    theme_ipsum_rc(grid="XY") + 
+                    theme(axis.title.x = element_blank(),text=element_text(size=16))
+                    ,
+                  gg_vax$plot[[2]]+labs(subtitle='B: 80+, 70-79, 60-69, EW, 50-59, ...') +
+                    scale_color_viridis(discrete = T)+
+                    theme_ipsum_rc(grid="XY"),
+                  ncol=2, nrow=1, common.legend=TRUE, legend="bottom")
+saveRDS(fig1a, "fig1a.rds")
+
+# Arrange
+fig1b = ggarrange(ggarrange(plotlist=trajA, nrow=2, ncol=2, widths = c(1,1,1,1),
+                            common.legend=TRUE, legend="bottom"),
+           #ggarrange(plotlist=trajB, nrow=1, ncol=4, widths = c(1,1,1,1)),
+           nrow=1)
+
+saveRDS(fig1b, "fig1b.rds")
+ggsave("figures/fig-trajectories.pdf", width = 45, height = 10, device = cairo_pdf)      
+
+ggarrange(fig1a, fig1b, nrow=2,heights = c(1, 1.6))
+ggsave("figures/fig-trajectoriesFull.pdf", width = 15, height = 10, device = cairo_pdf)      
+
+
+##########################
+# FIG 2 (bar plots)
+###########################
+tsize <- 16
+
+#---SUMMARIZE
+res2 <- res %>% 
+    group_by(type, R, ve,vp) %>%
+    nest() %>%
+    summarize(cases=map_dbl(data, total_cases),
+              hosp=map_dbl(data, total_hosp,hosp_efficacy=rep(vp, 9)),
+              deaths=map_dbl(data, total_deaths),
+              long = map_dbl(data, total_long, hosp_efficacy=rep(vp, 9)),
+              t_turn = map_dbl(data, time_to_decr),
+              cases_turn = map_dbl(data,cases_immunity),
+              vax_immunity = map_dbl(data, vax_immunity)
+)
+
+saveRDS(res2, "res2.rds")
+
+R_vec <- c(1.15,1.3) # R vals to plot
+
+g1 <- ggplot(filter(res2, R %in% R_vec), aes(x=ve, y=cases, group=type, fill=type))+
+  geom_col(position='dodge', alpha=1)+ 
+  #scale_fill_brewer(palette = "Dark2")+ 
+  facet_wrap(~ R,  ncol=4,labeller=label_both)+
+  #scale_color_brewer(palette = "Dark2")+
+  theme(text=element_text(size=16))+
+  theme(panel.spacing.x=unit(1.5, "lines"))+
+  labs(x='Efficacy against Infection', y='Infections', fill='Strategy') +
+            theme(text=element_text(size=tsize))+
+            theme(panel.spacing.x=unit(1.5, "lines"),
+                          axis.text.x = element_text(angle = 35,hjust = 1))+
+  scale_x_continuous(breaks=c(0.6,0.75,0.9)) +
+  scale_y_continuous(labels = scales::comma) +
+  theme_ipsum_rc(grid="Y")
+
+g2 <- ggplot(filter(res2, R %in% R_vec), aes(x=ve, y=hosp, group=type, fill=type))+
+  geom_col(position='dodge',alpha=1)+ 
+  #scale_fill_brewer(palette = "Dark2")+ 
+  facet_wrap(~ R,  ncol=4,labeller=label_both)+
+  #scale_color_brewer(palette = "Dark2")+
+  theme(text=element_text(size=tsize))+
+  theme(panel.spacing.x=unit(1.5, "lines"))+
+  labs(x='Efficacy against Infection', y='Hospitalizations', fill='Strategy') +
+            theme(text=element_text(size=16))+theme(panel.spacing.x=unit(1.5, "lines") ,
+                          axis.text.x = element_text(angle = 35,hjust = 1))+
+  scale_x_continuous(breaks=c(0.6,0.75,0.9)) + theme_ipsum_rc(grid="Y")
+
+g3 <- ggplot(filter(res2, R %in% R_vec), aes(x=ve, y=deaths, group=type, fill=type))+
+  geom_col(position='dodge',alpha=1)+ 
+  #scale_fill_brewer(palette = "Dark2")+ 
+  facet_wrap(~ R,  ncol=4, labeller=label_both)+
+  #scale_color_brewer(palette = "Dark2")+
+  theme(text=element_text(size=16))+
+  theme(panel.spacing.x=unit(1.5, "lines"))+
+  labs(x='Efficacy against Infection', y='Deaths', fill='Strategy') +
+            theme(text=element_text(size=tsize))+theme(panel.spacing.x=unit(1.5, "lines") ,
+                          axis.text.x = element_text(angle = 35,hjust = 1))+
+  scale_x_continuous(breaks=c(0.6,0.75,0.9)) + theme_ipsum_rc(grid="Y")
+
+g4 <- ggplot(filter(res2, R %in% R_vec), aes(x=ve, y=long, group=type, fill=type))+
+  geom_col(position='dodge',alpha=1)+ 
+  #scale_fill_brewer(palette = "Accent")+ 
+  facet_wrap(~ R,  ncol=4,labeller=label_both)+
+  #scale_color_brewer(palette = "Accent")+
+  theme(text=element_text(size=16))+
+  theme(panel.spacing.x=unit(1.5, "lines"))+
+  
+  labs(x='Efficacy against Infection', y='Long COVID', fill='Strategy') +
+            theme(text=element_text(size=tsize))+theme(panel.spacing.x=unit(1.5, "lines") ,
+                          axis.text.x = element_text(angle = 35,hjust = 1))+
+  scale_x_continuous(breaks=c(0.6,0.75,0.9)) +
+  theme_ipsum_rc(grid="Y") 
+
+bars <- ggarrange(g1,g2,g3,g4, ncol=2, nrow=2, common.legend=TRUE, legend='bottom', align='v')
+
+saveRDS(bars, "bars.rds")
+ggsave('figures/fig-barplots.pdf', width=14, height=10,  device = cairo_pdf)
+
+
+### Personal Risk
+
+oo <- compare_sims_data(sim1 = filter(res, R==1.3 & scen==1 & ve==0.75), 
+                                               sim2=filter(res, R==1.3 & scen ==2 & ve==0.75),
+                                               name1=labels[1], name2=labels[2], startDate=startDate, 
+                                               textsize = 16)
+
+
+
+
+oo %>% filter (age_band == "50-59" & scen == "B: 80+, 70-79, 60-69, EW, 50-59, ..." & 
+                 date<ymd("2021-06-01") & date>ymd("2021-04-01")) %>%
+  select(incid, newdeaths, hosp, long) %>% summarise_all(sum) / 709300
+
+oo %>% filter (age_band == "40-49" & scen == "B: 80+, 70-79, 60-69, EW, 50-59, ..." & 
+                 date<ymd("2021-06-01") & date>ymd("2021-04-01")) %>%
+  select(incid, newdeaths, hosp, long) %>% summarise_all(sum) / 617410
+
+oo %>% filter (age_band == "30-39" & scen == "B: 80+, 70-79, 60-69, EW, 50-59, ..." & 
+                 date<ymd("2021-06-01") & date>ymd("2021-04-01")) %>%
+  select(incid, newdeaths, hosp, long) %>% summarise_all(sum) / 607340
+
+oo %>% filter (age_band == "20-29" & scen == "B: 80+, 70-79, 60-69, EW, 50-59, ..." & 
+                 date<ymd("2021-06-01") & date>ymd("2021-04-01")) %>%
+  select(incid, newdeaths, hosp, long) %>% summarise_all(sum) / 590560
+
+
+####### Validation
+cases <- read_csv('http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Case_Details.csv')
+
+counts <- cases %>% filter (Reported_Date > ymd("2020-12-01")) %>% 
+  group_by(Reported_Date) %>% tally()
+
+
+observed <- as_tibble(counts) %>% rename(date = Reported_Date) %>%
+  mutate(observed = zoo::rollmean(n, k = 7, fill = NA))
+
+
+predicted <- oo %>% 
+  filter(scen == "B: 80+, 70-79, 60-69, EW, 50-59, ...") %>% 
+  group_by(date) %>% summarize(incid = sum(incid)) %>% 
+  filter (date < ymd("2021-04-05") & date > ymd("2021-01-02"))
+
+validation <- predicted %>% left_join(observed, by="date")
+validation %>% pivot_longer(cols = c(incid, observed)) %>%
+  ggplot () + geom_line(aes(y=value, x=date, color=name)) + 
+  theme_ipsum_rc(grid="Y")
+
+age.under.80 <- c("0-9", 
+                  "10-19",
+                  "20-29",
+                  "30-39",
+                  "40-49",
+                  "50-59",
+                  "60-69",
+                  "70-79")
+
+age.80.plus <- c("80-89", "90+")
+
+
+countsPerAge <- cases %>% filter (Reported_Date > ymd("2020-12-01")) %>% 
+  group_by(Reported_Date, Age_Group) %>% rename (age_band = Age_Group) %>%
+  mutate(age_band=recode(age_band,"<10" = "0-9")) %>%
+  mutate(age_band  = ifelse(age_band %in% age.under.80, age_band,
+                            ifelse(age_band %in% age.80.plus, "80+", 
+                                   NA))) %>%
+  tally() 
+
+
+predictedPerAge <- oo %>% 
+  filter(scen == "B: 80+, 70-79, 60-69, EW, 50-59, ...") %>% 
+  group_by(date, age_band) %>% summarize(incid = sum(incid)) %>% 
+  filter (date < ymd("2021-04-05") & date > ymd("2021-01-02"))
+
+observedPerAge <- as_tibble(countsPerAge) %>% rename(date = Reported_Date) %>%
+  mutate(observed7day = zoo::rollmean(n, k = 7, fill = NA))
+
+
+observedPerAge5daySum <- as_tibble(countsPerAge) %>% rename(date = Reported_Date) %>%
+  mutate(observed5daysum = 5* zoo::rollmean(n, k = 5, fill = NA))
+
+observedPerAge5daySum %>% filter(date==ymd("2021-01-01"))
+
+
+validationPerAge <- predictedPerAge %>% 
+  left_join(observedPerAge, by=c("date", "age_band"))
+validationPerAge %>% pivot_longer(cols = c(incid, observed7day)) %>%
+  ggplot () + geom_line(aes(y=value, x=date, color=name)) + 
+  facet_wrap(~age_band) +
+  theme_ipsum_rc(grid="Y")
+
+#####################
+# QALYs and Cost
+#####################
+#source('./analysis/qalys_cost.R')
---
+++ analysis/setup.R
@@ -0,0 +1,163 @@
+library(tidyverse)
+library(reshape2)
+library(RColorBrewer)
+library(furrr)
+library(cowplot)
+library(lubridate)
+library(ggpubr)
+library(viridis)
+library(hrbrthemes)
+
+no_cores <- availableCores() - 1
+plan(multicore, workers = no_cores)
+theme_set(theme_light())
+
+source('./vaccine-model.R') # main model & simulation funcs
+source('./utils.R') 
+source('./contact-matrix.R')
+
+# BUILD CONTACT MATRIX
+p_ess <- c(0.0,0.0,0.1705,0.2043,0.1675,0.1536,0.1642,0.1069, 0.0) 
+age_demo_by_five <- as.matrix(readr::read_csv('./data/Population_Estimates.csv'))
+generate_contact_matrix(p_ess, age_demo_by_five) #build
+
+age_demo <- readRDS("./generated-data/age_demographics_essential.rds")
+mu_home <- readRDS("./generated-data/mu_home_essential.rds")
+mu_work <- readRDS("./generated-data/mu_work_essential.rds")
+mu_school <- readRDS("./generated-data/mu_school_essential.rds")
+mu_other <-  readRDS("./generated-data/mu_other_essential.rds")
+
+# RATES (10-yr age bins, 0-9,10-19,...,80+)
+YLL_vec <- readRDS("./data/yll_vec_CAN.RData")
+IFR <- readRDS("./data/ifr_vec_CAN.RData")
+IHR <- readRDS("./data/ihr_vec_CAN.RData")
+
+# Symptom duration lognormal. log sigma=0.8. log  mu are 1.9, 2.2, 2.5, 2.8,
+# for ages under 30, 30-39, 40-49, over 50.
+# means are 9, 12, 17 and 22
+SDUR_vec = c(9,9,9, 12, 17, 22,22,22,22)  # symptom dur Sudre et al and Paul
+
+LCR = c(0.037, 0.037, 0.037, 0.079,0.15,0.252, 0.252, 0.252, 0.252) # from Paul from Sudre
+longDUR = c(rep(40.3,3), 42.5, 45.6, rep(49.7,4)) 
+HOSPDUR = c(4.692308,3.380952,5.260870,4.304348,7.212598,7.874346,9.092857,12.752475,12.964346)
+
+
+# ADD ESSENTIAL WORKERS TO IFR 
+IFR <- c(IFR, IFR[3:8])
+
+
+# GLOBAL MODEL PARAMETERS
+n <- length(age_demo)
+pop_total <- age_demo[n] #pop BC
+age_demo <- age_demo[1:n-1]
+N_i <-  pop_total*age_demo
+num_groups <- length(age_demo)
+
+
+sero_none <- rep(0, num_groups) # no prior immunity
+startDate <- lubridate::ymd("2021-01-01")
+
+I_0 <- c(133.0,
+         241.0,
+         488.8,
+         364.0,
+         315.2,
+         243.2,
+         203.2,
+         111.2,
+         155.0,
+         122.2, 
+         91.0,  
+         78.8,
+         60.8,  
+         50.8,  
+         27.8) # estimated prevalence by age in BC in Jan 2021
+
+u_var <- c(0.58, 0.34, 0.35, 0.34, 0.46, 0.45, 0.88, 1, 1,
+  0.79, 0.86, 0.8, 0.82, 1.0,1.0) # rel. susceptibility (this works better with validation than one in bubar)
+
+percent_vax <- 1.0 # just a limit we can't exceed
+
+#H = c(0.0,0.0,0.3,0.2,0.2,0.2,0.15,0.15,0.15,0.3,0.2,0.2,0.2,0.15,0.15)*N_i # Hesitancy
+H = c(0.0,0.0,0.3,0.2,0.2,0.2,0.15,0.20,0.20,
+      0.3,0.2,0.2,0.2,0.15,0.15)*N_i # Hesitancy. Increased for 80+ to match cases
+
+#H = c(0.0,0.0,0.3,0.2,0.2,0.2,0.01,0.01,0.01,0.3,0.2,0.2,0.2,0.01,0.01)*N_i # Hesitancy
+##########################
+# DEFINE MAIN STRATEGIES
+##########################
+
+labels <- c(#'A: Oldest to Youngest', 
+            'A: 80+, 70-79, EW, 60-69, ...', 
+            'B: 80+, 70-79, 60-69, EW, 50-59, ...')
+
+strategies <- list(#list(9, c(8,15),c(7,14), c(6,13), c(5,12), c(4,11), c(3,10)), 
+                   list(9, 8, 10:15, 7,6,5,4,3),
+                   list(9, 8, 7, 10:15, 6, 5, 4, 3))
+
+
+#######################
+# DEFINE MAIN SCENARIO
+# This is the main piecewise
+# scenario. Start with first phase
+# of vaccinating 80+ slowly. Then
+# move on to a second phase of
+# faster vaccination 
+########################
+run_over_scen_2 = function(R, ve, vp, scen,alpha=0.0){
+   T1 <- 60 #extended till April 1st, and adding 70 to 79
+   T2 <- 270 - T1
+   # Initial stage (vax all 80+)
+   R_init <- 1.00
+   n <- ( age_demo[9])/T1
+   C <- construct_C_from_prem(home=mu_home, work=mu_work, school=mu_school, other=mu_other, u=u_var,
+                              target_R0=R_init, in_school=TRUE, alpha_factor=alpha)
+
+   df0 <- run_sim_basic(C, I_0=I_0, percent_vax =1.0, strategy=list(9), num_perday=n,
+                        v_e = rep(ve, num_groups), v_p=rep(vp, num_groups),
+                        u = u_var, num_days=T1, with_essential=TRUE, H=H) 
+   # Final stage
+   n <- sum(age_demo[-c(9)])/T2
+   C <- construct_C_from_prem(home=mu_home, work=mu_work, school=mu_school, other=mu_other, u=u_var,
+                              target_R0=R, in_school=TRUE, alpha_factor=alpha)
+   df <- run_sim_restart(C, df_0=tail(df0, n=1), percent_vax =1.0, strategy= strategies[[scen]], num_perday=n,
+                         v_e = rep(ve, num_groups), v_p=rep(vp, num_groups),
+                         u = u_var, num_days=T2, with_essential=TRUE, H=H)
+   # combine 
+   df$time <- df$time+T1+1
+   df <- combine_age_groups(rbind(df0,df))
+   # add pars
+   df$R <- R
+   df$ve <- ve
+   df$vp <- vp
+   df$type <- labels[[scen]]
+   df$scen <- scen
+   df$alpha <- alpha
+   return(df)}
+
+
+########################
+# Another scenario
+# no piecewise
+# constant R and rate (n)
+########################
+
+run_over_scen = function(R, ve, vp, scen, n, alpha=0.0){
+   T <- 270 
+
+   C <- construct_C_from_prem(home=mu_home, work=mu_work, school=mu_school, other=mu_other, u=u_var,
+      target_R0=R, in_school=TRUE, alpha_factor=alpha)
+    
+   df <- run_sim_basic(C, I_0=I_0, percent_vax =1.0, strategy= alt_strategies[[scen]], num_perday=n,
+                           v_e = rep(ve, num_groups),v_p=rep(vp, num_groups),
+                           u = u_var, num_days=T, with_essential=TRUE, H=H)
+
+   df <- combine_age_groups(df)
+   df$R0 <- R
+   df$ve <- ve
+   df$vp <- vp
+   df$type <- alt_labels[[scen]]
+   df$scen <- scen
+   df$alpha <- alpha
+   return(df)}
+
